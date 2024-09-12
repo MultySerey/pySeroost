6f508771017bf3fc8f6646fdcac51dd0c61a60e9
MultySerey <multyserey@gmail.com>
2024-09-12 15:13:53 +0500

    idk2

--- main.py
+++ main.py
@@ -5,27 +5,45 @@ from typing import Dict
 
 
 def index_document(doc_content: str) -> Dict[str, int]:
-    raise NotImplementedError
+    try:
+        raise NotImplementedError
+    except NotImplementedError:
+        print("Not implemented")
+
+
+def parse_pdf(doc_name: str) -> str:
+    with open(doc_name, 'rb') as f:
+        return "".join([page.get_text() for page in pymupdf.Document(f)])
+
+
+def parse_docx(doc_name: str) -> str:
+    with open(doc_name, 'rb') as f:
+        return "\n".join([i.text for i in docx.Document(f).paragraphs])
 
 
 def read_doc(doc_name: str) -> str | None:
+    extension = path.splitext(doc_name)[-1]
     try:
-        with open(doc_name, 'rb') as f:
-            match path.splitext(doc_name)[-1]:
-                case ".docx":
-                    return "\n".join([i.text for i in docx.Document(f).paragraphs])
-                case '.pdf':
-                    return "".join([page.get_text() for page in pymupdf.Document(f)])
-                case _:
-                    return None
+        match extension:
+            case ".docx":
+                return parse_docx(doc_name)
+            case '.pdf':
+                return parse_pdf(doc_name)
+            case _:
+                return None
     except PermissionError as e:
         print(e)
         return None
 
 
 def main():
-    for f in listdir("."):
-        print(read_doc(f))
+    # HashMap[filePath, HashMap[String, usize]]
+    all_documents = Dict[str, Dict[str, int]]
+
+    for doc in listdir("."):
+        doc_content = read_doc(doc)
+        if doc_content:
+            index_document(doc_content)
 
 
 if __name__ == "__main__":
